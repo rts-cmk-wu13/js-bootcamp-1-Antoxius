c2f287cf877081d91461d149617cb8ea512ad714
Xenius Tolderlund <Xeniusawbt@gmail.com>
2025-01-28 09:39:31 +0100

    Tilføjet array blockQuotes og QuotesList

---
+++ script.js
@@ -0,0 +1,5 @@
+let blockQuotes = ["quote 1", "quote 2", "quote 3", "quote 4", "quote 5", "quote 6", "quote 7", "quote 8", "quote 9", "quote 10"];
+
+let quotesList = document.querySelector('.quotes-list');
+
+console.log(quotesList);
